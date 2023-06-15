31dcdce86edcc595c3a792eee09d978d12a11066
Hayk-Kazaryan <125143608+Hayk-Kazaryan@users.noreply.github.com>
2023-06-16 00:04:15 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Goto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            point1:counter += 1;
+            Console.WriteLine(counter);
+            if(counter == 10)
+            {
+                goto point2;
+            }
+            goto point1;
+        point2: Console.WriteLine("End");
+        }
+    }
+}
